f6bf88897ffb7a5b5c37c73c63b8d66d23ebadaa
yiplee <guoyinl@gmail.com>
2018-10-31 16:12:21 +0800

    Add: api to read mixin user id

---
+++ wallet/request_status.go
@@ -0,0 +1,34 @@
+package wallet
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/fox-one/foxgo/account"
+	"github.com/fox-one/foxgo/request"
+)
+
+type Status struct {
+	IsPinSet bool   `json:"isPinSet"`
+	MixinId  string `json:"mixinId"`
+	Fullname string `json:"fullname"`
+}
+
+func GetWalletStatus(ctx context.Context, token string) (*Status, error) {
+	data, err := request.Get(ctx, "wallet/mixinUserInfo", account.WithToken(token))
+	if err != nil {
+		return nil, err
+	}
+
+	resp := struct {
+		Data struct {
+			Status
+		} `json:"data"`
+	}{}
+
+	if err = json.Unmarshal(data, &resp); err != nil {
+		return nil, err
+	}
+
+	return &resp.Data.Status, nil
+}
