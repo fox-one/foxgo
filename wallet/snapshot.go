506c09b8a99a3913c7ec379b58b11c917627fbfa
yiplee <guoyinl@gmail.com>
2018-12-01 18:18:49 +0800

    price number

--- wallet/snapshot.go
+++ wallet/snapshot.go
@@ -1,12 +1,35 @@
 package wallet
 
+import (
+	"strings"
+)
+
+type PriceNumber string
+
+func (p *PriceNumber) MarshalJSON() ([]byte, error) {
+	text := []byte("\"")
+
+	if p == nil {
+		text = append(text, '0')
+	} else {
+		text = append(text, []byte(*p)...)
+	}
+
+	return append(text, '"'), nil
+}
+
+func (p *PriceNumber) UnmarshalJSON(b []byte) error {
+	*p = PriceNumber(strings.Trim(string(b), "\""))
+	return nil
+}
+
 type Snapshot struct {
-	SnapshotId string  `json:"snapshotId"`
-	AssetId    string  `json:"assetId"`
-	TraceId    string  `json:"traceId"`
-	Amount     float64 `json:"amount"`
-	Memo       string  `json:"memo"`
-	CreatedAt  int64   `json:"createdAt"`
+	SnapshotId string      `json:"snapshotId"`
+	AssetId    string      `json:"assetId"`
+	TraceId    string      `json:"traceId"`
+	Amount     PriceNumber `json:"amount"`
+	Memo       string      `json:"memo"`
+	CreatedAt  int64       `json:"createdAt"`
 
 	CounterUserId string `json:"counterUserId"`
 
